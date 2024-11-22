116bc54f0482b7c8dbeb6301ffae09e0582834cc
22110077 <22110077@student.hcmute.edu.vn>
2024-11-22 09:58:12 +0700

    Chuyển file login và register vào folder account cho dễ quản lý

--- routes/account.route.js
+++ routes/account.route.js
@@ -2,6 +2,7 @@ import express from 'express';
 
 const router = express.Router();
 
+// Tính viết đại á, hem biết nó có tác dụng gì hem nữa, xóa đi cũng được.
 router.get('/login', function(req,res){
     res.render('account/login');
 });
