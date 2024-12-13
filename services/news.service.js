a8487af84ae1bb2a50c0606c44157c0fae3d9f42
shannonnonshan <khanhminhdoan04@gmail.com>
2024-12-13 12:48:10 +0700

    pull news + writer

---
+++ services/news.service.js
@@ -0,0 +1,7 @@
+import express from 'express';
+import newsService from '../services/news.service.js'
+const router = express.Router();
+router.get('/', async function (req, res) {
+    
+});
+export default router;
