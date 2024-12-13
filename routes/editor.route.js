072ae6d43590ecf876b4e64242259312082c0304
shannonnonshan <khanhminhdoan04@gmail.com>
2024-12-13 11:33:15 +0700

    thanh công bên hông, editor router

--- routes/editor.route.js
+++ routes/editor.route.js
@@ -1,14 +1,11 @@
 import express from 'express';
 import editorService from '../service/editor.service.js';
 const router = express.Router();
-router.get('/newsstorage', async function (req,res) 
+router.get('/storage', async function (req,res) 
 {
     //await dung de móc được promise và dùng kèm với async
     //const list = await editorService.findAll();
     res.render('vwEditor/storage', 
-        // {
-        //    list:list
-        // }, 
         {layout: `role`});
 });
 export default router;
