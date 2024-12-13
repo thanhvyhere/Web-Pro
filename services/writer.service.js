a5d14b17dde517f0287eab0c77672dd449a5a0a6
shannonnonshan <khanhminhdoan04@gmail.com>
2024-12-13 12:48:10 +0700

    pull news + writer

--- services/writer.service.js
+++ services/writer.service.js
@@ -1,2 +1,10 @@
 
-// import db from '../utils/db.js'
+import express from 'express';
+//import accountService from '../services/writer.service'
+
+const router = express.Router();
+router.get('/write', function (req, res) { // hàm comeback, khi điều kiện thỏa thì chạy
+    res.render('vwWriter/write');
+});
+
+export default router;
