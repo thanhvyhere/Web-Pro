3c02bcfa08e86ffb0645278cbc64bb0ce4e47f63
shannonnonshan <khanhminhdoan04@gmail.com>
2024-12-23 11:16:48 +0700

    auto update publishday in database, modify dbs

--- main.js
+++ main.js
@@ -8,6 +8,8 @@ import activate_view_middleware from './middleware/view.mdw.js';
 import activate_route_middleware from './middleware/routes.mdw.js';
 import activate_session_middleware from './middleware/session.mdw.js';
 import passport from 'passport';
+import cron from 'node-cron';
+import axios from 'axios'
 const app = express();
 
 const __dirname = dirname(fileURLToPath(import.meta.url)); 
@@ -31,6 +33,15 @@ activate_session_middleware(app);
 activate_locals_middleware(app);
 activate_view_middleware(app);
 activate_route_middleware(app);
+
+cron.schedule('* * * * *', async () => {
+    console.log('Cron job started...');
+    try {
+        const response = await axios.get('http://localhost:3000/newspaper/update-status');
+    } catch (error) {
+        console.error('Cron job error:', error.message);
+    }
+});
 
 app.listen(3000, function () {
     console.log('App is running at http://localhost:3000');
