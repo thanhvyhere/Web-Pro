2ac579701eaf3d5e08c33904525722e92ff4e351
shannonnonshan <khanhminhdoan04@gmail.com>
2025-06-07 15:37:53 +0700

    fix some model

--- model/Category.js
+++ model/Category.js
@@ -5,7 +5,7 @@ const { Schema, model } = mongoose;
 
 const categorySchema = new Schema({
     CatName: { type: String, required: true },
-    parent_id: { type: Number, default: null }
+    parent_id: { type: Number, ref: 'Category', default: null }
 });
 const Category = model('Category',categorySchema);
 export {Category};
